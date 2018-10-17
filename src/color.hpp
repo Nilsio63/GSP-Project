e4f096d29246fc4309f88202279ed9b8ee9f43a5
Nils <nilsbalke@t-online.de>
2018-10-17 11:51:37 +0200

    changed color typ from int to char

--- src/color.hpp
+++ src/color.hpp
@@ -1,11 +1,17 @@
 #ifndef COLOR_HPP_
 #define COLOR_HPP_
 
+const char COLOR_MAX = 255;
+
 struct Color {
-    int R;
-    int G;
-    int B;
-    int A;
+	unsigned char r;
+	unsigned char g;
+	unsigned char b;
+	unsigned char a;
+
+	Color() : r(0), g(0), b(0), a(COLOR_MAX) {}
+	Color(unsigned char r, unsigned char g, unsigned char b) : r(r), g(g), b(b), a(COLOR_MAX) {}
+	Color(unsigned char r, unsigned char g, unsigned char b, unsigned char a) : r(r), g(g), b(b), a(a) {}
 };
 
 #endif  // COLOR_HPP_

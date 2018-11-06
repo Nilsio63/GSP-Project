3f519c14bac59b0eac52a2b81481b069ef1b3f06
Nils <nilsbalke@t-online.de>
2018-11-06 15:38:11 +0100

    corrected vertex attributes
    corrected vertex attributes in geometry class

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -21,6 +21,7 @@ Geometry::Geometry(Color c) : color_(c)
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), nullptr);
+	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
 }
 

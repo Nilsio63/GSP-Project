5a6c53390d16ca64f7122fc9efd90a81e92bb3a5
ToBo <Thomas.Bollmann@studmail.w-hs.de>
2019-02-19 22:24:27 +0100

    New game

--- src/world.cpp
+++ src/world.cpp
@@ -20,6 +20,21 @@ void LogShader(int shaderId, char *shaderName)
 	delete(buffer);
 }
 
+void World::LoadLight()
+{
+	for (int i = 0; i < 6; i++)
+	{
+		defaultProgram_.SetUniform(("pointLights[" + std::to_string(i) + "].position").c_str(), lights_[i].position.x, lights_[i].position.y, lights_[i].position.z);
+		defaultProgram_.SetUniform(("pointLights[" + std::to_string(i) + "].ambient").c_str(), lights_[i].ambient.x, lights_[i].ambient.y, lights_[i].ambient.z);
+		defaultProgram_.SetUniform(("pointLights[" + std::to_string(i) + "].diffuse").c_str(), lights_[i].diffuse.x, lights_[i].diffuse.y, lights_[i].diffuse.z);
+		defaultProgram_.SetUniform(("pointLights[" + std::to_string(i) + "].specular").c_str(), lights_[i].specular.x, lights_[i].specular.y, lights_[i].specular.z);
+		defaultProgram_.SetUniform(("pointLights[" + std::to_string(i) + "].constant").c_str(), lights_[i].constant);
+		defaultProgram_.SetUniform(("pointLights[" + std::to_string(i) + "].linear").c_str(), lights_[i].linear);
+		defaultProgram_.SetUniform(("pointLights[" + std::to_string(i) + "].quadratic").c_str(), lights_[i].quadratic);
+		defaultProgram_.SetUniform(("pointLights[" + std::to_string(i) + "].lightOn").c_str(), lights_[i].lightOn);
+	}
+	
+}
 World::~World()
 {
 	defaultProgram_.~ShaderProgram();
@@ -31,19 +46,18 @@ void World::Render()
 
 	glm::vec3 skyColor = glm::vec3(0.5f, 0.5f, 0.5f);
 
+
 	defaultProgram_.SetUniform("skyColor", skyColor.x, skyColor.y, skyColor.z);
 	defaultProgram_.SetUniform("dirLight.direction", 1, -1, -1);
 	defaultProgram_.SetUniform("dirLight.ambient", 0.1f, 0.1f, 0.1f);
 	defaultProgram_.SetUniform("dirLight.diffuse", 0.3f, 0.3f, 0.3f);
 	defaultProgram_.SetUniform("dirLight.specular", 0.6f, 0.6f, 0.6f);
 
-	defaultProgram_.SetUniform("pointLight.position", 3, -10, 2);
-	defaultProgram_.SetUniform("pointLight.ambient", 0.1f, 0.0f, 0.0f);
-	defaultProgram_.SetUniform("pointLight.diffuse", 0.5f, 0.0f, 0.0f);
-	defaultProgram_.SetUniform("pointLight.specular", 0.75f, 0.0f, 0.0f);
-	defaultProgram_.SetUniform("pointLight.constant", 0.1f);
-	defaultProgram_.SetUniform("pointLight.linear", 0.045f);
-	defaultProgram_.SetUniform("pointLight.quadratic", 0.0075f);
+	//Light anschalten
+	lights_[2].lightOn = 1;
+
+	LoadLight();
+	
 
 	player_.Render(&defaultProgram_);
 
@@ -53,7 +67,7 @@ void World::Render()
 	}
 	
 	glm::mat4 skyview = glm::mat4(glm::mat3(GetCamera()->GetView()));
-	//skybox_.Render(&skyboxProgram_, skyview, GetCamera()->GetProjektion(), skyColor);
+	skybox_.Render(&skyboxProgram_, skyview, GetCamera()->GetProjektion(), skyColor);
 
 	LogShader(defaultProgram_.GetVertexShader().GetShaderId(), "Vertex Shader");
 	LogShader(defaultProgram_.GetFragmentShader().GetShaderId(), "Fragment Shader");
@@ -86,7 +100,7 @@ void World::LoadModel()
 
 	////Figuren
 	//Model* F1 = new Model("../models/Elements/F/F1/Export/F1.obj");
- //AddModel("F1", F1);
+	//AddModel("F1", F1);
 	//Model* F2 = new Model("../models/Elements/F/F2/Export/F2.obj");
 	//AddModel("F2", F2);
 
@@ -133,32 +147,32 @@ void World::LoadModel()
 	//Licht
 	Model* L1 = new Model("../models/Elements/L/L1/Export/L1.obj");
 	AddModel("L1", L1);
-	Model* L2 = new Model("../models/Elements/L/L2/Export/L2.obj");
-	AddModel("L2", L2);
-	Model* L3 = new Model("../models/Elements/L/L3/Export/L3.obj");
-	AddModel("L3", L3);
-	Model* L5 = new Model("../models/Elements/L/L5/Export/L5.obj");
-	AddModel("L5", L5);
+	//Model* L2 = new Model("../models/Elements/L/L2/Export/L2.obj");
+	//AddModel("L2", L2);
+	//Model* L3 = new Model("../models/Elements/L/L3/Export/L3.obj");
+	//AddModel("L3", L3);
+	//Model* L5 = new Model("../models/Elements/L/L5/Export/L5.obj");
+	//AddModel("L5", L5);
 	Model* L6 = new Model("../models/Elements/L/L6/Export/L6.obj");
 	AddModel("L6", L6);
-	Model* L7 = new Model("../models/Elements/L/L7/Export/L7.obj");
-	AddModel("L7", L7);
+	//Model* L7 = new Model("../models/Elements/L/L7/Export/L7.obj");
+	//AddModel("L7", L7);
 	Model* L8 = new Model("../models/Elements/L/L8/Export/L8.obj");
 	AddModel("L8", L8);
 
 	//nav
 	Model* NL1 = new Model("../models/Elements/L/L1/Export/L1_nav.obj");
 	AddModel("NL1", NL1);
-	Model* NL2 = new Model("../models/Elements/L/L2/Export/L2_nav.obj");
-	AddModel("NL2", NL2);
-	Model* NL3 = new Model("../models/Elements/L/L3/Export/L3_nav.obj");
-	AddModel("NL3", NL3);
-	Model* NL5 = new Model("../models/Elements/L/L5/Export/L5_nav.obj");
-	AddModel("NL5", NL5);
+	//Model* NL2 = new Model("../models/Elements/L/L2/Export/L2_nav.obj");
+	//AddModel("NL2", NL2);
+	//Model* NL3 = new Model("../models/Elements/L/L3/Export/L3_nav.obj");
+	//AddModel("NL3", NL3);
+	//Model* NL5 = new Model("../models/Elements/L/L5/Export/L5_nav.obj");
+	//AddModel("NL5", NL5);
 	Model* NL6 = new Model("../models/Elements/L/L6/Export/L6_nav.obj");
 	AddModel("NL6", NL6);
-	Model* NL7 = new Model("../models/Elements/L/L7/Export/L7_nav.obj");
-	AddModel("NL7", NL7);
+	//Model* NL7 = new Model("../models/Elements/L/L7/Export/L7_nav.obj");
+	//AddModel("NL7", NL7);
 	Model* NL8 = new Model("../models/Elements/L/L8/Export/L8_nav.obj");
 	AddModel("NL8", NL8);
 
@@ -177,20 +191,36 @@ void World::LoadModel()
 	AddModel("NK2", NK2);
 	Model* NK3 = new Model("../models/Elements/K/K3/Export/K3_nav.obj");
 	AddModel("NK3", NK3);
-	Model* NK4 = new Model("../models/Elements/K/K4/Export/K4_nav.obj");
-	AddModel("NK4", NK4);
+	//Model* NK4 = new Model("../models/Elements/K/K4/Export/K4_nav.obj");
+	//AddModel("NK4", NK4);
 
 }
+
+void World::CreateLight(int lightNr,glm::vec3 pos)
+{
+	Light temp;
+	temp.position = pos;
+	temp.ambient = glm::vec3(0.1f, 0.1f, 0.1f);
+	temp.diffuse = glm::vec3(0.3f, 0.3f, 0.3f);
+	temp.specular = glm::vec3(0.6f, 0.6f, 0.6f);
+	temp.constant = 0.1f;
+	temp.linear = 0.045f;
+	temp.quadratic = 0.0075f;
+	lights_[lightNr] = temp;
+}
+
 void World::CreateInstances()
 {
-	for (int i = 0; i <10; i++)
+	int lightNr = 0;
+	for (int i = 0; i <20; i++)
 	{
-		for (int j = 0; j < 10 ; j++)
+		for (int j = 0; j < 20 ; j++)
 		{
 			std::string typ = worldLoader_.map[i][j].substr(0, 2);
 			int ausrichtung = std::stoi(worldLoader_.map[i][j].substr(2, 1));
 
 			//Modell
+
 			Instance * m = new Instance(models_.find(typ)->second);
 			
 			m->Translate(glm::vec3(5 * i, 0, 5 * j));
@@ -199,15 +229,33 @@ void World::CreateInstances()
 	
 			AddInstance(m);
 
-			typ = "N" + typ;
-			//Nav
-			Instance * n = new Instance(models_.find(typ)->second);
+			if (typ == "G8")
+			{
+				goal_ = glm::vec3(5 * i, 0, 5 * j);
+			}
+			if (typ == "G9")
+			{
+				start_ = glm::vec3(5 * i, 0, 5 * j);
+			}
 
-			n->Translate(glm::vec3(5 * i, 0, 5 * j));
+			if (typ.substr(0, 1) == "L")
+			{
+				CreateLight(lightNr,glm::vec3(5 * i, 0, 5 * j));
+				lightNr++;
+			}
 
-			n->Rotate(90 * ausrichtung, glm::vec3(0, 1, 0));
+			if (typ != "K4")
+			{
+				typ = "N" + typ;
+				//Nav
+				Instance * n = new Instance(models_.find(typ)->second);
 
-			AddNavInstance(n);
+				n->Translate(glm::vec3(5 * i, 0, 5 * j));
+
+				n->Rotate(90 * ausrichtung, glm::vec3(0, 1, 0));
+
+				AddNavInstance(n);
+			}
 		}
 	}
 }
--- src/world.hpp
+++ src/world.hpp
@@ -12,7 +12,7 @@
 
 #include <vector>
 #include <map>
-
+#include "light.hpp"
 #include <GL/glew.h>
 
 class World
@@ -27,8 +27,12 @@ private:
 	std::map<std::string, Model *> models_;
 	std::vector<Instance *> instances_;
 	std::vector<Instance *> navinstances_;
+	Light lights_[6];
+	glm::vec3 goal_;
+	glm::vec3 start_;
+
 public:
-	World() : defaultProgram_("../src/VertexShaderCode.glsl", "../src/FragmentShaderCode.glsl"), skyboxProgram_("../src/SkyboxVSCode.glsl", "../src/SkyboxFSCode.glsl") { worldLoader_.LoadMap("../map/Map_mittel.csv"); LoadModel(); CreateInstances(); }
+	World() : defaultProgram_("../src/VertexShaderCode.glsl", "../src/FragmentShaderCode.glsl"), skyboxProgram_("../src/SkyboxVSCode.glsl", "../src/SkyboxFSCode.glsl") { worldLoader_.LoadMap("../map/Map_V3.csv"); LoadModel(); CreateInstances(); }
 	~World();
 
 	Camera *GetCamera() { return player_.GetCamera(); }
@@ -36,9 +40,11 @@ public:
 	void Render();
 	void LoadModel();
 	void CreateInstances();
+	void CreateLight(int lightNr, glm::vec3 pos);
 	void AddModel(std::string name, Model *model);
 	void AddInstance(Instance *instance);
 	void AddNavInstance(Instance *instance);
+	void LoadLight();
 };
 
 #endif  // WORLD_HPP_
